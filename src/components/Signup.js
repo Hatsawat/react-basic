54caa3ae2a11fa437ce099d5c251b7a5698b9b8e
hatsawat <hatsawat@gmail.com>
2020-07-16 10:18:42 +0700

    first commit

---
+++ src/components/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <h2>สมัครมาชิก</h2>  
+            </div>
+        )
+    }
+}
